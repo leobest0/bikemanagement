41eca7f8988a6a0d9b684e8c46fad1874d3efb96
leobest0 <gaoming_deng@163.com>
2017-04-25 17:33:06 +0800

    4-25
    添加了代码

--- bqbikemanagement/js/homePage.js
+++ bqbikemanagement/js/homePage.js
@@ -1,3 +1,11 @@
 /**
  * Created by admin on 2017/4/19.
  */
+window.onload=function(){
+    $('.partner').on('click',function(){
+        $('.partner').css('color','#222');
+        $('.partner').css('background-color','#fff');
+        $(this).css('color','#fff');
+        $(this).css('background-color','#109ba3');
+    });
+}
